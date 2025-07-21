3b92cfe9bed799ce69e80ec4e42db5667d0ba371
Natalia <natalianory19@gmail.com>
2025-07-21 13:34:28 +0200

    First commit

---
+++ src/blackjack/usecases/index.js
@@ -0,0 +1,7 @@
+//Se crea este archivo para almacenar todos los ''import'' que estaba en el index principal porque asi solo se tiene que pasar uno en el principal
+
+export {crearDeck} from './crear-deck';
+export { pedirCarta } from './pedir-carta';
+export { valorCarta } from './valor-carta';
+export {turnoComputadora} from './turno-computadora';
+export {crearCartaHTML} from './crear-carta-html';
